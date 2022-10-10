d59637361b8be98273c9fbd145195fe8ca00e28c
Muklis Faridho Novianto <105256925+muklisfn@users.noreply.github.com>
2022-10-10 14:15:26 +0700

    Add files via upload

---
+++ Rental-Mobil/Rental/bukutamu.php
@@ -0,0 +1,13 @@
+<?php
+
+    $nama = $_POST["nama"];
+    $email = $_POST["email"];
+    $nohp = $_POST["nohp"];
+    $pesan = $_POST["pesan"];
+
+    echo $_POST["nama"]."<br>";
+    echo $email."<br>";
+    echo $nohp."<br>";
+    echo $pesan."<br>";
+
+?>
